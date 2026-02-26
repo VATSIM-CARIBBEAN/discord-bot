dbd478ffe6dc01691f9225f3cfbbabada8813ee9
- <zexagonal.selvo@gmail.com>
2026-02-25 17:50:34 -0800

    tech team role in zsu server has forcesync command ability

--- guilds/zsu/commands/Moderation/forcesync.js
+++ guilds/zsu/commands/Moderation/forcesync.js
@@ -1,13 +1,21 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { performRosterUpdate } = require('../../functions/scheduleRosterUpdate');
+
+const ALLOWED_ROLE_ID = '985574770785525802';
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('forcesync')
-    .setDescription('Force a roster sync for all server members.')
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    .setDescription('Force a roster sync for all server members.'),
 
   async execute(interaction) {
+    const hasRole = interaction.member.roles.cache.has(ALLOWED_ROLE_ID);
+    const isAdmin = interaction.member.permissions.has('Administrator');
+
+    if (!hasRole && !isAdmin) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    }
+
     await interaction.deferReply({ ephemeral: true });
 
     try {
